2373db9f1ada11937e2905718642e524c996b057
Muhanned Noman <Muhanned.Noman@gmail.com>
2021-04-26 01:56:52 +0300

    Feat: add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="wrap">
+        <div className="logo">
+          <h1>Logo</h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
