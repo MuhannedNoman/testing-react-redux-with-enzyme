40191f6dccf21218e5c12fb0b0b561aca3dfe9d3
Muhanned Noman <Muhanned.Noman@gmail.com>
2021-04-26 02:29:38 +0300

    Chore(dep): setup enzym for test

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import Enzyme from 'enzyme';
+import EnzymAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({
+  adapter: new EnzymAdapter(),
+  disableLifecycleMethods: true,
+});
