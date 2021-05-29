b51ebffc73421ce6f667855602d7eaa73e1b7838
Muhanned Noman <Muhanned.Noman@gmail.com>
2021-05-29 05:46:06 +0300

    Feat: connect App with store
    This will allow us to fetch data from API, and display
    the articles in the screen.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
 import Header from './components/Header';
 import Headline from './components/Headline';
+import Button from './components/Button';
+import ArticleCard from './components/ArticleCard';
+import {  fetchPosts  } from './actions';
 import './app.scss';
 
 const user = [
@@ -12,7 +18,27 @@ const user = [
   },
 ];
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.fetch = this.fetch.bind(this);
+  }
+
+   fetch () {
+    this.props.fetchPosts()
+  }
+
+  render() {
+    const {posts} = this.props;
+
+    const articles = posts.map((post,index) => {
+      const {title, body} = post;
+      return(
+        <ArticleCard key={index} title={title} desc={body} />
+      )
+    })
+
   return (
     <div className="App">
       <Header />
@@ -22,9 +48,23 @@ function App() {
           desc="Click the button to render posts"
           user={user}
         />
+        <Button event={this.fetch} text="Get posts" />
+        {posts.length > 0 && 
+          <div>
+            {articles}
+          </div>
+        }
       </section>
     </div>
   );
 }
 
-export default App;
+}
+
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps, {fetchPosts})(App);
