c9d04b41755b788f56a4a96e43e6b107feba36cc
Muhanned Noman <Muhanned.Noman@gmail.com>
2021-05-29 12:03:11 +0300

    Test(Button): emit click event function
    This will allow us to test the click event on the button
    component.

--- src/Tests/components/Button/Button.test.js
+++ src/Tests/components/Button/Button.test.js
@@ -17,10 +17,12 @@ describe('Button Component', () => {
 
   describe('Renders', () => {
     let component;
+    let mockFunc;
     beforeEach(() => {
+      mockFunc = jest.fn();
       const props = {
         text: 'Example',
-        event: () => {},
+        event: mockFunc,
       };
       component = shallow(<Button {...props} />);
     });
@@ -28,6 +30,13 @@ describe('Button Component', () => {
     it('Should Render a button', () => {
       const button = findByTestAttr(component, 'btn-component');
       expect(button.length).toBe(1);
+    });
+
+    it('Should emit callback on click event', () => {
+      const button = findByTestAttr(component, 'btn-component');
+      button.simulate('click');
+      const callback = mockFunc.mock.calls.length;
+      expect(callback).toBe(1);
     });
   });
 });
